4df48e588949fb72faf0f5f8981232e233acc7ce
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2024-11-19 12:48:08 +0000

    Update day1.rs

--- aoc2015/src/day1.rs
+++ aoc2015/src/day1.rs
@@ -1,3 +1,19 @@
 pub fn run(input: &str) {
-    println!("2015 day1");
+    let mut curr_pos: isize = 0;
+    let mut has_entered_basement: bool = false;
+
+    for (i, c) in input.chars().enumerate() {
+        match c {
+            '(' => curr_pos += 1,
+            ')' => curr_pos -= 1,
+            _ => panic!("Invalid input"),
+        }
+
+        if !has_entered_basement && curr_pos == (-1 as isize) {
+            let basement_pos = i + 1;
+            println!("First entered basement at position `{:?}`", basement_pos);
+            has_entered_basement = true;
+        }
+    }
+    println!("Final position: {}", curr_pos);
 }
