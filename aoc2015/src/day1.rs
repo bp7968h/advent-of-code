dee7443ca3282ef7acb033eebb33ab5c07c4cf5a
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2024-11-19 13:22:26 +0000

    feat: complete day1 of year 2015
    * Update day1.rs
    
    * Update Cargo.toml
    
    * Run the specific module
    
    * Create day1 input file
    
    * write tests
    
    * refact: dismiss the returned value

--- aoc2015/src/day1.rs
+++ aoc2015/src/day1.rs
@@ -1,3 +1,51 @@
-pub fn run(input: &str) {
-    println!("2015 day1");
+pub fn run(input: &str) -> isize {
+    let mut curr_pos: isize = 0;
+    let mut has_entered_basement: bool = false;
+
+    for (i, c) in input.chars().enumerate() {
+        match c {
+            '(' => curr_pos += 1,
+            ')' => curr_pos -= 1,
+            _ => panic!("Invalid input"),
+        }
+
+        if !has_entered_basement && curr_pos == (-1 as isize) {
+            let basement_pos = i + 1;
+            println!("First entered basement at position `{:?}`", basement_pos);
+            has_entered_basement = true;
+        }
+    }
+    println!("Final position: {}", curr_pos);
+    
+    position
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+            
+    #[test]
+    fn test_floor_0() {
+        assert_eq!(run("(())"), 0);
+        assert_eq!(run("()()"), 0);
+    }
+            
+    #[test]
+    fn test_floor_3() {
+        assert_eq!(run("((("), 3);
+        assert_eq!(run("(()(()("), 3);
+        assert_eq!(run("))((((("), 3);
+    }
+            
+    #[test]
+    fn test_floor_minus_1() {
+        assert_eq!(run("())"), -1);
+        assert_eq!(run("))("), -1);
+    }
+            
+    #[test]
+    fn test_floor_minus_3() {
+        assert_eq!(run(")))"), -3);
+        assert_eq!(run(")())())"), -3);
+    }
 }
