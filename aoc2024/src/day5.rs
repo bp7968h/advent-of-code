2f690f4784de1f88c812165272abc4b439e95a73
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2024-12-07 20:32:18 +0000

    feat: solve day5 part1

--- aoc2024/src/day5.rs
+++ aoc2024/src/day5.rs
@@ -1,16 +1,42 @@
+use std::collections::HashMap;
+use std::collections::VecDeque;
+
 pub fn run(input: &str) {
-    // read rules
-    let rules: &str = r#""#;
-    let ord_rules: Vec<(usize, usize)> = rules.lines().map(|o| {
-        let mut parts = o.split('|');
-        let left = parts.next().unwrap().parse::<usize>().expect("ord left parse failed");
-        let right = parts.next().unwrap().parse::<usize>().expect("ord right parse failed");
-        (left, right)
+   let mut adj_list: HashMap<usize, Vec<usize>> = HashMap::new();
+    for line in ORDRULES.lines() {
+        let mut parts = line.split('|');
+        let left = parts.next().unwrap().parse::<usize>().expect("left parse failed");
+        let right = parts.next().unwrap().parse::<usize>().expect("right parse failed");
+        
+        adj_list.entry(left).or_insert_with(|| vec![]).push(right);
+    }
+    
+    let updates: Vec<Vec<usize>> = UPDATES.lines().map(|l| {
+        l.split(',').map(|e| e.parse::<usize>().expect("update parse failed")).collect()
     }).collect();
     
-    let updates: Vec<Vec<usize>> = input.lines().map(|u| {
-        u.split(',').map(|i| i.parse::<usize>().expect("update parse failed")).collect()
-    }).collect();
+    let mid_page_sum: usize = updates.iter().filter_map(|u| {
+        if does_follow_rules(&adj_list, &u) {
+            let midpoint = u.len() / 2;
+            return Some(u[midpoint]);
+        }
+        None
+    }).sum();
     
-    
+    println!("Sum: {}", mid_page_sum);
+}
+
+fn does_follow_rules(rules: &HashMap<usize, Vec<usize>>, update: &[usize]) -> bool {
+    let mut visited: Vec<usize> = Vec::new();
+    for u in update {
+        if let Some(after_list) = rules.get(&u) {
+            for item in after_list {
+                if visited.contains(item) {
+                    return false
+                }
+            }
+        }
+        visited.push(*u);
+    }
+    true
 }
