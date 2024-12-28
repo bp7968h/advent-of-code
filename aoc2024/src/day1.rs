f2b6244c4342c24d99b72cbd818b25de4556b1fc
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2024-12-28 20:12:34 +0000

    fix: make day1 compatible with runner

--- aoc2024/src/day1.rs
+++ aoc2024/src/day1.rs
@@ -16,29 +16,41 @@ pub fn run(input: &str) {
     }
     assert_eq!(left_v.len(), right_v.len());
 
+    let part_one_sum = part_one(&left_v, &right_v);
+    println!("Part one puzzle answer: {part_one_sum}");
 
+    let part_two_sum = part_two(&left_v, &right_v);
+    println!("Part two puzzle answer: {part_two_sum}");
+
+}
+
+fn part_one(left_nums: &[usize], right_nums: &[usize]) -> usize {
+    let mut left_sorted: Vec<usize> = left_nums.to_owned();
+    let mut right_sorted: Vec<usize> = right_nums.to_owned();
+    left_sorted.sort();
+    right_sorted.sort();
+
+    let mut sum: usize = 0;
+    for i in 0..left_sorted.len() {
+        sum += left_sorted[i].abs_diff(right_sorted[i]);
+    }
+
+    sum
+}
+
+fn part_two(left_nums: &[usize], right_nums: &[usize]) -> usize {
     let mut records: HashMap<usize, usize> = HashMap::new();
     let mut sum: usize = 0;
     
-    for i in left_v.iter() {
+    for i in left_nums.iter() {
         if records.contains_key(i) {
             sum += i * records.get(&i).unwrap();
             continue;
         }
-        let count = right_v.iter().filter(|n| *n == i).count();
+        let count = right_nums.iter().filter(|n| *n == i).count();
         sum += i * count;
         records.insert(*i, count);
     }
-    println!("Sum: {:?}", sum);
     
-    // left_v.sort();
-    // right_v.sort();
-    
-    // assert_eq!(left_v.len(), right_v.len());
-    // let mut sum: u64 = 0;
-    // for i in 0..left_v.len() {
-    //     sum += left_v[i].abs_diff(right_v[i]);
-    // }
-    
-    // println!("Sum: {:?}", sum);
+    sum
 }
