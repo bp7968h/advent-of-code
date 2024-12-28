f8e4b70940b86fb1440db82041e77aca1f015a3d
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2024-12-28 21:12:34 +0000

    refact/test: extract functions, add test

--- aoc2024/src/day1.rs
+++ aoc2024/src/day1.rs
@@ -4,6 +4,18 @@ pub fn run(input: &str) {
     let mut left_v: Vec<usize> = vec![];
     let mut right_v: Vec<usize> = vec![];
     
+    parse_input(input, &mut left_v, &mut right_v);
+    assert_eq!(left_v.len(), right_v.len());
+
+    let part_one_sum = part_one(&left_v, &right_v);
+    println!("Part one puzzle answer: {part_one_sum}");
+
+    let part_two_sum = part_two(&left_v, &right_v);
+    println!("Part two puzzle answer: {part_two_sum}");
+
+}
+
+fn parse_input(input: &str, left_v: &mut Vec<usize>, right_v: &mut Vec<usize>) {
     for line in input.lines() {
         let mut line_iter = line.split_whitespace();
         
@@ -14,15 +26,7 @@ pub fn run(input: &str) {
         left_v.push(left.parse::<usize>().unwrap());
         right_v.push(right.parse::<usize>().unwrap());
     }
-    assert_eq!(left_v.len(), right_v.len());
-
-    let part_one_sum = part_one(&left_v, &right_v);
-    println!("Part one puzzle answer: {part_one_sum}");
-
-    let part_two_sum = part_two(&left_v, &right_v);
-    println!("Part two puzzle answer: {part_two_sum}");
-
-}
+} 
 
 fn part_one(left_nums: &[usize], right_nums: &[usize]) -> usize {
     let mut left_sorted: Vec<usize> = left_nums.to_owned();
@@ -53,4 +57,31 @@ fn part_two(left_nums: &[usize], right_nums: &[usize]) -> usize {
     }
     
     sum
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn from_example() {
+        let input: &str = r#"3   4
+4   3
+2   5
+1   3
+3   9
+3   3"#;
+
+        let mut left_input: Vec<usize> = vec![];
+        let mut right_input: Vec<usize> = vec![];
+
+        parse_input(input, &mut left_input, &mut right_input);
+        assert_eq!(left_input.len(), right_input.len());
+
+        let part_one_sum = part_one(&left_input, &right_input);
+        assert_eq!(part_one_sum, 11);
+
+        let part_two_sum = part_two(&left_input, &right_input);
+        assert_eq!(part_two_sum, 31);
+    }
 }
